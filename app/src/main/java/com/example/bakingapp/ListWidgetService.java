6989cb057878b187402c7b864e88f6e92824359f
mark.bukovich46@gmail.com <mark.bukovich46@gmail.com>
2021-02-02 23:10:04 -0600

    Added Comments and Member Variables to ListWidgetService
    Added two member variables to RecipeRemoteViewsFactory
    which is in the same file as ListWidgetService. Also
    added some comments and TODOs that will help in filling
    out the class.

--- app/src/main/java/com/example/bakingapp/ListWidgetService.java
+++ app/src/main/java/com/example/bakingapp/ListWidgetService.java
@@ -5,16 +5,19 @@ import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
+import java.util.List;
+
 public class ListWidgetService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return null;
+        return new RecipeRemoteViewsFactory(this.getApplicationContext());
     }
 }
 
 class RecipeRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     Context mContext;
+    static List<Recipe> mRecipes;
 
     public RecipeRemoteViewsFactory(Context context) {
         mContext = context;
@@ -25,9 +28,10 @@ class RecipeRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory 
 
     }
 
+    // this is called on start and when notifyAppWidgetViewDataChanged is called
     @Override
     public void onDataSetChanged() {
-
+        // TODO get data from network resource
     }
 
     @Override
@@ -35,13 +39,18 @@ class RecipeRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory 
 
     }
 
+    // Returns the number of items necessary for the GridView to display
     @Override
     public int getCount() {
+        // TODO return the correct number of items for the GridView
         return 0;
     }
 
+    // This very important method is where the data is actually binded to the views.
+    // Very similar to onBindViewHolder in a collection Adapter
     @Override
     public RemoteViews getViewAt(int position) {
+        // TODO bind data to views
         return null;
     }
 
